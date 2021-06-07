faac62069ab9a44fd5a4f42f20b57712305faa90
funduleadiana <diana.fundulea@yahoo.com>
2021-06-07 12:47:30 +0100

    setting values

--- src/tables/UserTable.js
+++ src/tables/UserTable.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const UserTable=()=> (
+const UserTable=(props)=> (
     <table>
         <thead>
             <tr>
@@ -11,14 +11,22 @@ const UserTable=()=> (
             </tr>
         </thead>
         <tbody> 
-            <tr>
-                <td>Name data</td>
-                <td>User data</td>
-                <td>
-                    <button className="button">Edit</button>
-                    <button className="button">Delete</button>
-                </td>
-            </tr>
+                {props.users.length > 0? (
+                    props.users.map((user)=> (
+                        <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>{user.user}</td>
+                        <td>
+                        <button className="button">Edit</button>
+                        <button className="button">Delete</button>
+                        </td>
+                        </tr>
+                        ))
+                    ): (
+                        <tr>
+                            <td colSpan={3}>No users</td>
+                        </tr>
+                    )}
         </tbody>
     
     </table>
